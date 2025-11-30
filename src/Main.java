ea3ab49236858f0c43798ad6608fd3e99b55cf62
AndreiPavlov1988 <ai_pavlov@mail.ru>
2025-11-30 18:15:51 +0300

    correction

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,38 @@
-// Class Author
-public class Author {
+public class Main {
+    public static void main(String[] args) {
+
+        Author author1 = new Author("Лев", "Толстой");
+        Author author2 = new Author("Федор", "Достоевский");
+
+
+        Book book1 = new Book("Война и мир", author1, 1867);
+        Book book2 = new Book("Преступление и наказание", author2, 1866);
+
+
+        System.out.println("Информация о книгах:");
+        printBookInfo(book1);
+        printBookInfo(book2);
+
+
+        System.out.println("Изменяем год публикации книги 'Война и мир'...");
+        book1.setPublicationYear(1869);
+
+
+        System.out.println("Обновленная информация о книгах:");
+        printBookInfo(book1);
+        printBookInfo(book2);
+    }
+
+    public static void printBookInfo(Book book) {
+        System.out.println("Книга:" + book.getTitle() + "'");
+        System.out.println("Автор:" + book.getAuthor().getFirstName() + " " + book.getAuthor().getLastName());
+        System.out.println("Год издания:" + book.getPublicationYear());
+        System.out.println();
+    }
+}
+
+
+class Author {
     private String firstName;
     private String lastName;
 
@@ -16,8 +49,8 @@ public class Author {
         return lastName;
     }
 }
-// Class Book
-public class Book {
+
+class Book {
     private String title;
     private Author author;
     private int publicationYear;
